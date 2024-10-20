3e9af04c0c966fd04d04b3f5212fe818ee63c509
Jander Gomez <jandergb.30gmail.com>
2024-10-21 01:14:57 +0200

    feat: create orders & docker & prisma

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class OrdersService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('Orders Service');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  create(createOrderDto: CreateOrderDto) {
+    return 'This action adds a new order';
+  }
+
+  findAll() {
+    return `This action returns all orders`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} order`;
+  }
+
+}
